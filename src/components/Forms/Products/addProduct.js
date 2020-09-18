98a06d539573635f98b70184cc45616736caba28
Ridho Abdul Majid <ridhoabdulmajid@gmail.com>
2020-09-18 21:09:34 +0700

    filestack

--- src/components/Forms/Products/addProduct.js
+++ src/components/Forms/Products/addProduct.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import {
@@ -13,140 +13,128 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useDispatch } from "react-redux";
 import { addProduct } from "../../../redux/actions";
 import { useHistory } from "react-router-dom";
-import jwtDecode from "jwt-decode";
 import ReactFilestack from "filestack-react";
-const filestackapi= "AugqfuGzTQouENQs5OOe2z";
+const filestackapi = "AugqfuGzTQouENQs5OOe2z";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     boxShadow: "0 0.7rem 1rem rgba(111, 115, 184, 0.8) !important",
   },
   field: {
-    width: "100%",
+    width: "600px",
   },
   error: {
     color: "red",
     fontStyle: "italic",
   },
+  button: {
+    margin: theme.spacing(1),
+    backgroundColor: '#d16473'
+},
 }));
 
 export default function AddProduct() {
   const classes = useStyles();
   const dispatch = useDispatch();
   const history = useHistory();
-  // const loggedAdmin = jwtDecode(localStorage.getItem('token'))
-  // console.log(loggedAdmin.role, "logged")
 
-  const CustomField = (props) => {
-    return (
-      <TextField
-        fullWidth
-        variant="outlined"
-        margin="normal"
-        className={classes.field}
-        required
-        {...props}
-      />
-    );
+  let [values, setValues] = useState({
+    productName: "",
+    categories: "",
+    price: "",
+    stock: "",
+    image: {
+      id: "",
+      image: "",
+    },
+  });
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
   };
-
-  const filestack = (props) => {
-    return (
-      <ReactFilestack
-        apikey={`${filestackapi}`}
-        onSuccess={(result) => {
-          this.setState({
-            imageUrl: result.filesUploaded[0].url,
-          });
-        }}
-      />
-    );
+  const handleSubmit = (event) => {
+    console.log(event);
+    event.preventDefault();
+    dispatch(addProduct(values, history));
   };
 
   return (
-    <Container>
-      <Formik
-        initialValues={{
-          productName: "",
-          categories: "",
-          price: "",
-          stock: "",
-          image: {
-            id: '',
-            urlImage: ''
-          }
+    <Container className={classes.field}>
+      <FormControl onSubmit={handleSubmit}>
+        <Grid
+          container
+          justify="center"
+          direction="column"
+          alignItems="center"
+          spacing={2}
+        >
+            <TextField
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              className={classes.field}
+              id="standard-full-width"
+              label="Product Name"
+              name="productName"
+              value={values.productName}
+              onChange={handleChange}
+            />
 
-        }}
-        onSubmit={(values) => {
-          dispatch(addProduct(values, history));
-        }}
-      >
-        {() => (
-          <Form noValidate autoComplete="off">
-            <Grid
-              container
-              justify="center"
-              direction="column"
-              alignItems="center"
-              spacing={2}
-            >
-              <Grid container item xs={12} md={6} lg={6}>
-                <Field
-                  type="productName"
-                  as={CustomField}
-                  name="productName"
-                  label="Product Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid container item xs={12} md={6} lg={6}>
-                <Field
-                  type="text"
-                  as={CustomField}
-                  name="categories"
-                  label="Categories"
-                  autoFocus
-                />
-              </Grid>
-              <Grid container item xs={12} md={6} lg={6}>
-                <Field
-                  type="text"
-                  as={CustomField}
-                  name="price"
-                  label="Price"
-                />
-              </Grid>
-              <Grid container item xs={12} md={6} lg={6}>
-                <Field
-                  type="text"
-                  as={CustomField}
-                  name="stock"
-                  label="Stock"
-                />
-              </Grid>
-              <Grid container item xs={12} md={6} lg={6}>
-                <Field
-                  type="text"
-                  as={CustomField}
-                  name="image.image"
-                  label="image"
-                />
-              </Grid>
-              <Grid container item xs={12} md={6} lg={6}>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className={classes.submit}
-                >
-                  Add Product
-                </Button>
-              </Grid>
-            </Grid>
-          </Form>
-        )}
-      </Formik>
+          <TextField
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            className={classes.field}
+            id="standard-full-width"
+            label="Categories"
+            name="categories"
+            value={values.categories}
+            onChange={handleChange}
+          />
+
+          <TextField
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            className={classes.field}
+            id="standard-full-width"
+            label="price"
+            name="price"
+            value={values.price}
+            onChange={handleChange}
+          />
+
+          <TextField
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            className={classes.field}
+            id="standard-full-width"
+            label="Stock"
+            name="stock"
+            value={values.stock}
+            onChange={handleChange}
+          />
+          <ReactFilestack
+            apikey={`${filestackapi}`}
+            onSuccess={(result) => {
+              setValues({
+                ...values,
+                image: {
+                  id: "",
+                  image: result.filesUploaded[0].url,
+                },
+              });
+            }}
+          />
+        <br/>
+          <Button className={classes.button} variant="outlined" color="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Grid>
+      </FormControl>
     </Container>
   );
 }
